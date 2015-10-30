889ea46e87dd5559b49abe789eb5472059f4eee5
JD Crouch <jdcrouch5@gmail.com>
2015-10-31 01:44:30 +0900

    Create CSV file of entry data

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,35 +1,38 @@
 # #8
 require_relative 'entry'
+require 'csv'
 
 class AddressBook
 
     attr_accessor :entries
-    
+
     def initialize
         @entries = []
     end
-    
+
     def add_entry(name, phone_number, email)
-        # #9
+
         index = 0
         @entries.each do |entry|
-            # #10
+
             if name < entry.name
                 break
             end
             index += 1
         end
-        # #11
+
         @entries.insert(index, Entry.new(name, phone_number, email))
-        
+
     end
-    
+
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks:true)
+
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+    end
+
 end
-
-#8 Here we tell Ruby to load the library named entry.rb relative to address_book.rb's file path using require_relative.
-
-#9 Here we create a variable to store the insertion index.
-
-#10 Here we compare name with the name of the current entry. If name lexicographically proceeds entry.name, we've found the index to insert at. Otherwise we increment index and continue comparing with the other entries.
-
-#11 Here we insert a new entry into entries using the calculated index.
